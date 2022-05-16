03e9000094e43c5effecec39b8eb356cbc66b4fe
callaghan <hattinghcallaghan@gmail.com>
2022-05-16 21:49:30 +0200

    update all open orders test

--- test/test_api.py
+++ test/test_api.py
@@ -1,27 +1,22 @@
-import requests
-import time
-from src.auth import sign_request
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from src.api import get_all_open_orders, post_limit_order, del_order
 
 
 def test_get_all_open_orders():
-    timestamp = int(time.time() * 1000)
-    verb = "GET"
-    path = "/v1/orders/open"
-    signature = sign_request(timestamp, verb, path)
-
-    url = "https://api.valr.com/v1/orders/open"
-    payload = {}
-    headers = {
-        "X-VALR-API-KEY": os.getenv("API_KEY"),
-        "X-VALR-SIGNATURE": f"{signature}",
-        "X-VALR-TIMESTAMP": f"{timestamp}",
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-
+    response = get_all_open_orders()
     assert response.status_code == 200
 
 
+def test_create_and_delete_post_order():
+    # side = 'BUY'
+    # amount = 0.001
+    # price = 10000
+    # custom_id = 'pytest-trading-bot-4'
+    # create = post_limit_order(side, amount, price, custom_id)
+    # delete = del_order(customer_id=custom_id)
+    # print(1)
+    # print(create.text)
+    # print(2)
+    # print(delete.text)
+    # assert create.status_code == 200
+    # assert delete.status_code == 200
+    pass
