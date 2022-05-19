b4702a83ee3adbdb7d604dca78dce6436b84ae66
callaghan <hattinghcallaghan@gmail.com>
2022-05-19 13:39:16 +0200

    test sign_request

--- test/test_api.py
+++ test/test_api.py
@@ -1,4 +1,14 @@
 from src.api import get_all_open_orders, post_limit_order, del_order
+from src.auth import sign_request
+
+
+def test_sign_request():
+    response = sign_request(1652958074138, "GET", "/v1/orders/open")
+    print(response)
+    assert (
+        "037220009189eda4fdd1a43fcc96f9bf642e1a93803de25746f3d1e73e37b908"
+        "6513c07bcc94b99a2d570f1868cde670310160f48eda20b1b8cd75b6361150f8" == response
+    )
 
 
 def test_get_all_open_orders():
