efc04183a255f951c994c26f85ff442e721c3c56
callaghan <hattinghcallaghan@gmail.com>
2022-06-11 15:21:51 +0200

    delete orders

--- src/calcu/buy.py
+++ src/calcu/buy.py
@@ -1,6 +1,20 @@
-def orders_cancelled():
+from src.calcu.info import buy_list_open_orders_to_delete, buy_list_orders_to_place
+from src.calcu.calcu import open_orders, buy_list_id
+from src.api import post_limit_order, del_order
+
+def orders_delete():
+    d = buy_list_open_orders_to_delete(open_orders, buy_list_id)
+    if not d:
+        return True
+    else:
+        for i in d:
+            print(i)
+            del_order(customer_id=i)
+            pass
+        return False
     pass
 
 
 def orders_placed():
+    buy_list_orders_to_place(buy_list_id)
     pass
